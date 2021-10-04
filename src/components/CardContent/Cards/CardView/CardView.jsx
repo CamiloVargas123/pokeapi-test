98f99e36229f48d4cdf98c642cf4e418aa2c2025
Camilo Vargas <camilovargasingm@gmail.com>
2021-10-04 17:52:30 -0500

    card view

---
+++ src/components/CardContent/Cards/CardView/CardView.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./CardView.scss"
+
+export default function CardView(props) {
+  const {pokemon} = props
+  return (
+    <div className="card">
+      <h2>{pokemon.name}</h2>
+      <img src={pokemon.sprites.front_default} alt={`${pokemon.name}.png`} />
+      <div className="card_type">
+        {pokemon.types.map((types, index) => {
+          return <p key={index} className={types.type.name}>{types.type.name}</p>
+        })}
+      </div>
+    </div>
+  )
+}
