e0e265fa52c21b3f44379fe48b6be1ac110689e3
Camilo Vargas <camilovargasingm@gmail.com>
2021-10-04 17:53:54 -0500

    add new funtion and rename old funtion

--- src/services/GetPoke.js
+++ src/services/GetPoke.js
@@ -1,7 +1,24 @@
 let urlMain = "https://pokeapi.co/api/v2/"
 
-export function searchPokemon(pokemon) {
+export function searchPokemonAPI(pokemon) {
   let url = `${urlMain}pokemon/${pokemon}`
+  return fetch(url).then(reponse => {
+    return reponse.json();
+  }).catch(err => {
+    return err
+  })
+}
+
+export function getPokemonAPI(limit=10, offset=0){
+  let url = `${urlMain}pokemon?limit=${limit}&offset=${offset}`
+  return fetch(url).then(reponse => {
+    return reponse.json();
+  }).catch(err => {
+    return err
+  })
+}
+
+export function getPokemonWhitUrlAPI(url){
   return fetch(url).then(reponse => {
     return reponse.json();
   }).catch(err => {
