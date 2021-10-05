737df8b49a09fd283159e3f5b0e149e0f4c23681
Camilo Vargas <camilovargasingm@gmail.com>
2021-10-05 16:18:47 -0500

    implement infinitiScroll

--- src/components/CardContent/CardContent.jsx
+++ src/components/CardContent/CardContent.jsx
@@ -3,22 +3,31 @@ import "./CardContent.scss"
 import Cards from './Cards/Cards'
 import ViewDetails from './ViewDetails/ViewDetails'
 import {getPokemonAPI, getPokemonWhitUrlAPI} from "../../services/GetPoke"
+import InfiniteScroll from 'react-infinite-scroll-component'
+import Loading from '../Loading/Loading'
 
 export default function CardContent() {
   const [pokemons, setPokemons] = useState([])
+  const [page, setPage] = useState(0)
   const [viewDatail, setViewDetail] = useState(false)
+  const [loading, setLoading] = useState(true)
 
-  const getPokemons = async () => {
+  const getPokemons = async (page = 0) => {
     try{
-      const data = await getPokemonAPI()
+      const data = await getPokemonAPI(25, page)
       const promise = data.results.map( async (pokemon) => {
         return await getPokemonWhitUrlAPI(pokemon.url)
       })
       const result = await Promise.all(promise)
-      setPokemons(result)
+      setPokemons(pokemons.concat(result))
+      setLoading(false)
     }catch(err){
       console.log(err)
     }
+  }
+  const fechtMorePokemons = (page) => {
+    setPage(page)
+    getPokemons(page)
   }
 
   useEffect(() => {
@@ -26,9 +35,18 @@ export default function CardContent() {
   }, [])
 
   return (
-    <div className="cardContent">
-      <Cards pokemons={pokemons} />
+    <>
+      <InfiniteScroll
+        className="cardContent"
+        dataLength={pokemons.length}
+        next={() => fechtMorePokemons(page + 25)}
+        hasMore={true}
+        loader={<Loading />}
+      >
+        <Cards pokemons={pokemons} loading={loading} getPokemons={getPokemons} page={page} />
+      </InfiniteScroll>
+
       {viewDatail ? <ViewDetails /> : null}
-    </div>
+    </>
   )
 }
